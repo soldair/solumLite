ea607642aae1a97225900e70148de97ddf4ac867
Ryan Day <soldair@gmail.com>
2010-05-29 02:44:46 -0700

    adding super light benchmark class previous published as gist http://gist.github.com/397664

--- htdocs/php/framework/bench.class.php
+++ htdocs/php/framework/bench.class.php
@@ -1,38 +1,58 @@
-<?php
-/* copyright ryan day 2010 */
+<?php /*quick and useful benchmarking static class
+copyright Ryan Day 2010
+http://ryanday.org
 
+lgpl
+
+USE:
+bench::mark('all');
+sleep(1);
+bench::mark('app');
+sleep(3);
+bench::end('app');
+sleep(1);
+bench::endAll();
+
+echo bench::total('app')." seconds spent in app";
+echo bench::total('all')." seconds spent in all";
+*/
 class bench{
-	private static $marks = array();
+        private static $marks = array();
+        private static $mark_structure = array('total'=>0,'start'=>0);
 
-	public static function mark($mark,$started_from_microtime = false){
-		if($started_from_microtime) $s = $started_from_microtime;
-		else $s = microtime(true);
+        public static function mark($name,$start = null){
+                self::verify($name);
+                $time = microtime(true);
+                if(isset($start)) $time = $start;
+                self::$marks[$name]['start'] = $time;
+        }
 
-		self::$marks[$mark] = array('start'=>$s);
-	}
+        public static function end($name) {
+                if(isset(self::$marks[$name])){
+                        if(self::$marks[$name]['start']){
+                                self::$marks[$name]['total'] += microtime(true)-self::$marks[$name]['start'];
+                                self::$marks[$name]['start'] = 0;
+                        }
+                }
+        }
 
-	public static function end($mark){
-		if(isset(self::$marks[$mark])) self::$marks[$mark]['end'] = microtime(true);
-	}
+        public static function total($name){
+                self::verify($name);
+                return self::$marks[$name]['total'];
+        }
 
-	public static function elapsed($mark){
-		if(isset(self::$marks[$mark])){
-			if(isset(self::$marks[$mark]['end'])) $t = self::$marks[$mark]['end'];
-			else $t = microtime(true);
+        public static function endAll(){
+                foreach(self::$marks as $name=>&$data){
+                        if($data['start']){
+                                $data['total'] += microtime(true)-$data['start'];
+                                $data['start'] = 0;
+                        }
+                }
+        }
 
-			return $t-self::$marks[$mark]['start'];
-		}
-		return false;
-	}
-
-	public static function elapsed_ms($mark){
-		if($t = self::elapsed($mark)){
-			return sprintf('%0.5f',$t*1000);
-		}
-		return false;
-	}
-
-	public static function get_marks(){
-		return array_keys(self::$marks);
-	}
+        private static function verify($name){
+                if(!isset(self::$marks[$name])){
+                        self::$marks[$name] = self::$mark_structure;
+                }
+        }
 }
